77bde5553eaa5d18c35d619a5a404afea860b507
Nikita Bekish <zinczinc1997@mail.ru>
2019-06-02 20:42:45 +0300

    add page profile

--- src/services/instaservice.js
+++ src/services/instaservice.js
@@ -18,6 +18,16 @@ export default class InstaService {
         return res;
     }
 
+    getAllPhotos = async () => {
+        const res = await this.getResource('posts/');
+        return res.map(this._transformPosts);
+    }
     
-    
+    _transformPosts = (post) => {
+        return {
+            src: post.src,
+            alt: post.alt,
+            id: post.id
+        }
+    }
 }
