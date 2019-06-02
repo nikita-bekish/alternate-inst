21eeae11007e28a2ed54cf9f22fbec1223bf923e
Nikita Bekish <zinczinc1997@mail.ru>
2019-06-02 18:00:46 +0300

    add feed from json server

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,14 +1,74 @@
 import React, {Component} from 'react';
-import Post from './Post';
+import User from './User';
+import InstaService from '../services/instaservice';
+import ErrorMessage from './ErrorMessage';
 
 export default class Posts extends Component {
+    InstaService = new InstaService();
+    state = {
+        posts: [],
+        error: false
+    }
+
+    componentDidMount() {
+        this.updatePosts();
+    }
+
+    updatePosts() {
+        this.InstaService.getAllPosts()
+        .then(this.onPostsLoaded)
+        .catch(this.onError)
+    }
+
+    onPostsLoaded = (posts) => {
+        this.setState({
+            posts,
+            error: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map((item) => {
+            const {name, altname, photo, src, alt, descr, id} = item;
+
+            return (
+                <div key={id} className="post">
+                    <User 
+                        src={photo}
+                        alt={altname}
+                        name={name}
+                        min/>
+                    <img src={src} alt={alt}></img>
+                    <div className="post__name">
+                        {name}
+                    </div>
+                    <div className="post__descr">
+                        {descr}
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
+        const {error, posts} = this.state;
+
+        if (error) {
+            return <ErrorMessage />
+        }
+
+        const items = this.renderItems(posts);
+
         return (
             <div className="left">
-                <Post alt="nature" src="https://www.9skips.com/wp-content/uploads/2017/10/forests.jpg" />
-                <Post alt="nature1" src="https://www.nature.com/polopoly_fs/7.44180.1495028629!/image/WEB_GettyImages-494098244.jpg_gen/derivatives/landscape_630/WEB_GettyImages-494098244.jpg" />
-                <Post alt="nature2" src="https://www.visitportugal.com/sites/www.visitportugal.com/files/mediateca/23_660x371.jpg" />
+                {items}
             </div>
-        );
+        )
     }
 }
